2b1e38842f09d9a279a530fd6877eb656dfeb9aa
Bena <benajs@gmail.com>
2019-07-10 10:39:56 +0530

    feed_changes

---
+++ db/migrate/20190709082742_add_columns_to_feed.rb
@@ -0,0 +1,12 @@
+class AddColumnsToFeed < ActiveRecord::Migration[6.0]
+  def change
+    add_column :feeds, :site, :string
+    add_column :feeds, :isXML, :boolean
+    add_column :feeds, :summary, :string
+    add_column :feeds, :content, :string
+    add_column :feeds, :author, :string
+    add_column :feeds, :category, :string
+    add_column :feeds, :published, :string
+    add_column :feeds, :others, :string
+  end
+end
